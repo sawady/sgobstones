571ada3ce27f5f2bc6e210ff3e6aabbefe825b7c
sawady.faso <sawady.faso@8a6f59b0-c97f-5f57-74eb-8be8df638f9c>
2014-03-10 21:25:43 +0000

    Added probar

--- sgobstones/Gobstones/src/Gobstones/Console.scala
+++ sgobstones/Gobstones/src/Gobstones/Console.scala
@@ -4,6 +4,8 @@ trait Console extends App with CommonLanguage {
   
   def imprimir(x: Any) = println(x)
   
+  def probar(msg: String, p: Boolean) = imprimir(msg + ": " + p)
+  
   def main()
   
   main()
